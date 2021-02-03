325f1d78c80cdc3f439cbc8e92b42440eada840b
Soory Ranganathan <soory.ranga@hotmail.com>
2021-02-02 22:08:21 -0500

    let there be light
    Signed-off-by: Arun Ranganathan <arun.ranga@hotmail.ca>

---
+++ server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.SetLevel(log.DEBUG)
+	e.Logger.Fatal(e.Start(":1323"))
+}
