511ed33cc8bb6382b8d18c31e9a17addb85e5d96
Soory Ranganathan <soory.ranga@hotmail.com>
2021-02-06 16:32:11 -0500

    custom validation for payload

--- go.mod
+++ go.mod
@@ -4,8 +4,11 @@ go 1.15
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/labstack/echo/v4 v4.1.17
 	github.com/labstack/gommon v0.3.0
+	github.com/leodido/go-urn v1.2.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 )
--- src/auth/login.go
+++ src/auth/login.go
@@ -13,9 +13,9 @@ import (
 
 // Types definition
 const (
-	Google   = "GOOGLE"
-	LinkedIn = "LINKEDIN"
-	Email    = "Email"
+	GOOGLE   = "google"
+	LINKEDIN = "linkedin"
+	EMAIL    = "email"
 )
 
 // jwtCustomClaims are custom claims extending default ones.
@@ -28,12 +28,38 @@ type jwtCustomClaims struct {
 type (
 	// LoginPayload is the struct used to hold payload from /login
 	LoginPayload struct {
-		Email    string `json:"email" validate:"email"`
+		Email    string `json:"email" validate:"omitempty,email"`
 		Token    string `json:"token"`
-		Password string `json:"password" validate:"gte=10,required_with=email"`
-		Type     string `json:"type" validate:"required,oneof= GOOGLE LINKEDIN EMAIL"`
+		Password string `json:"password" validate:"omitempty,gte=10"`
+		Type     string `json:"type" validate:"required,oneof= google linkedin email"`
 	}
 )
+
+// LoginPayloadValidation validates user inputs
+func LoginPayloadValidation(sl validator.StructLevel) {
+
+	payload := sl.Current().Interface().(LoginPayload)
+
+	switch payload.Type {
+	case GOOGLE:
+		if len(payload.Token) == 0 {
+			sl.ReportError(payload.Token, "token", "Token", "validToken", "")
+		}
+	case LINKEDIN:
+		if len(payload.Token) == 0 {
+			sl.ReportError(payload.Token, "token", "Token", "validToken", "")
+		}
+	case EMAIL:
+		if len(payload.Email) == 0 {
+			sl.ReportError(payload.Email, "email", "Email", "validEmail", "")
+		}
+		if len(payload.Password) < 10 {
+			sl.ReportError(payload.Email, "password", "Password", "validPassworrd", "")
+		}
+	}
+
+	// plus can do more, even with different tag than "fnameorlname"
+}
 
 func loginEmail(c echo.Context) (string, error) {
 	email := "arun.ranga@hotmail.ca"
@@ -79,24 +105,24 @@ func loginLinkedIn(c echo.Context) (string, error) {
 
 // Login is used to login an user
 func Login(c echo.Context) error {
-	payload := new(LoginPayload)
+	payload := LoginPayload{}
 
-	if err := c.Bind(payload); err != nil {
+	if err := c.Bind(&payload); err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
-	if err := validator.Validate(payload); err != nil {
+	if err := c.Validate(&payload); err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 	var token string
 	var error error
 
 	switch payload.Type {
-	case Google:
+	case GOOGLE:
 		token, error = loginGoogle(c)
-	case LinkedIn:
+	case LINKEDIN:
 		token, error = loginLinkedIn(c)
-	case Email:
+	case EMAIL:
 		token, error = loginEmail(c)
 	default:
 		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Login type - %s is not supported", payload.Token))
