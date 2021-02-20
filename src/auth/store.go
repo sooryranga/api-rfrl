6dbcbee9270d1016d009b5b62c40232a451b5577
Soory Ranganathan <soory.ranga@hotmail.com>
2021-02-20 10:36:59 -0500

    add client table and able to create new user

--- src/auth/store.go
+++ src/auth/store.go
@@ -1,57 +1,64 @@
 package auth
 
 import (
+	"github.com/Arun4rangan/api-tutorme/src/client"
 	"github.com/Arun4rangan/api-tutorme/src/db"
 )
 
 const (
 	getByToken string = `
-SELECT client.*, password FROM auth 
-JOIN client ON auth.id == client.id
+SELECT client.* FROM auth 
+JOIN client ON auth.client_id = client.id
 WHERE auth.token =$1 AND auth.auth_type =$2 
 LIMIT 1
 	`
 	getByEmail string = `
-SELECT * FROM auth 
-JOIN client ON auth.id == client.id
+SELECT password_hash, client.* FROM auth 
+JOIN client ON auth.client_id = client.id
 WHERE auth.email =$1 AND auth.auth_type =$2 
 LIMIT 1
 	`
 	insertEmailAuth string = `
-INSERT INTO auth (email, password_hash, auth_type)
-VALUES ($1, $2, $3)
+INSERT INTO auth (email, password_hash, auth_type, client_id)
+VALUES ($1, $2, $3, $4)
 RETURNING id
 	`
 	insertToken string = `
-INSERT INTO auth (token, auth_type) 
-VALUES ($1, $2) 
+INSERT INTO auth (token, auth_type, client_id) 
+VALUES ($1, $2, $3) 
 RETURNING id
 	`
 )
 
 // GetByToken queries the database for token auth from providers
-func GetByToken(db db.DB, token string, authType string) (*Auth, error) {
-	var m Auth
-	err := db.QueryRowx(getByToken, token, authType).StructScan(&m)
+func GetByToken(db db.DB, token string, authType string) (*client.Client, error) {
+	var c client.Client
+	err := db.QueryRowx(getByToken, token, authType).StructScan(&c)
 	if err != nil {
 		return nil, err
 	}
-	return &m, nil
+	return &c, nil
 }
 
 // GetByEmail queries the database for email auth
-func GetByEmail(db db.DB, email string) (*Auth, error) {
-	var m Auth
-	err := db.QueryRowx(getByEmail, email, EMAIL).StructScan(&m)
-	if err != nil {
-		return nil, err
+func GetByEmail(db db.DB, email string) (*client.Client, []byte, error) {
+
+	type getByEmailStruct struct {
+		client.Client
+		PasswordHash []byte `db:"password_hash"`
 	}
-	return &m, nil
+	var result getByEmailStruct
+
+	row := db.QueryRowx(getByEmail, email, EMAIL)
+
+	err := row.StructScan(&result)
+
+	return &result.Client, result.PasswordHash, err
 }
 
 // CreateWithEmail creates auth row with email in db
 func CreateWithEmail(db db.DB, auth *Auth, clientID string) (int, error) {
-	row := db.QueryRowx(insertEmailAuth, auth.Email, auth.PasswordHash, EMAIL)
+	row := db.QueryRowx(insertEmailAuth, auth.Email, auth.PasswordHash, EMAIL, clientID)
 	var id int = -1
 
 	err := row.Scan(&id)
@@ -61,7 +68,7 @@ func CreateWithEmail(db db.DB, auth *Auth, clientID string) (int, error) {
 
 // CreateWithToken creates auth row with token in db
 func CreateWithToken(db db.DB, auth *Auth, clientID string) (int, error) {
-	row := db.QueryRowx(insertToken, auth.Token, auth.AuthType)
+	row := db.QueryRowx(insertToken, auth.Token, auth.AuthType, clientID)
 	var id int = -1
 	err := row.Scan(&id)
 
--- src/auth/use_cases.go
+++ src/auth/use_cases.go
@@ -2,10 +2,9 @@ package auth
 
 import (
 	"database/sql"
-	"time"
+	"fmt"
 
 	"github.com/Arun4rangan/api-tutorme/src/client"
-	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/pkg/errors"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -26,13 +25,13 @@ func (h *Handler) signupWithToken(newClient *client.Client, auth *Auth) (*client
 		return nil, err
 	}
 
-	_, err = CreateWithToken(h.db, auth, newClient.ID)
+	_, err = CreateWithToken(tx, auth, newClient.ID)
 
 	if err != nil {
 		if rb := tx.Rollback(); rb != nil {
 			return nil, errors.Wrap(rb, err.Error())
 		}
-		return nil, err
+		return nil, errors.Wrap(err, fmt.Sprintf("%v", newClient.ID))
 	}
 
 	err = tx.Commit()
@@ -95,7 +94,7 @@ func (h *Handler) signupEmail(
 	hash, err := hashAndSalt([]byte(password))
 
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	newClient := client.NewClient(firstName, lastName, about, email, photo)
@@ -138,48 +137,28 @@ func (h *Handler) signupEmail(
 }
 
 func (h *Handler) loginEmail(email string, password string) (*client.Client, error) {
-	auth, err := GetByEmail(h.db, email)
+	c, passwordHash, err := GetByEmail(h.db, email)
 
 	if err != nil {
-		return "", err
+		return nil, errors.Wrap(err, "GetByEmail")
 	}
 
 	err = bcrypt.CompareHashAndPassword(
-		auth.PasswordHash,
+		passwordHash,
 		[]byte(password),
 	)
 
 	if err != nil {
-		return "", err
+		return nil, errors.Wrap(err, "CompareHashAndPassword")
 	}
 
-	return client.GetClientFromID(h.db, auth.ClientID)
+	return c, nil
 }
 
-func (h *Handler) loginGoogle(token string) (string, error) {
-	auth, err := GetByToken(h.db, token, GOOGLE)
-
-	if err != nil {
-		return "", err
-	}
-
-	return "", err
+func (h *Handler) loginGoogle(token string) (*client.Client, error) {
+	return GetByToken(h.db, token, GOOGLE)
 }
 
-func (h *Handler) loginLinkedIn(token string) (string, error) {
-	_, err := GetByToken(h.db, token, LINKEDIN)
-
-	if err != nil {
-		return "", err
-	}
-
-	claims := &JWTClaims{
-		"",
-		true,
-		jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(time.Hour * 72).Unix(),
-		},
-	}
-
-	return h.GenerateToken(claims)
+func (h *Handler) loginLinkedIn(token string) (*client.Client, error) {
+	return GetByToken(h.db, token, LINKEDIN)
 }
