557be075e5fbcd2117ce4b95bdce6dbcfc008d11
Arun Ranganathan <arun.ranga@hotmail.ca>
2021-02-28 20:07:50 -0500

    WIP: refactor into https://medium.com/@benbjohnson/standard-package-layout-7cdbc8391fc1 arch

--- tutorme/document.go
+++ tutorme/document.go
@@ -1,4 +1,4 @@
-package document
+package tutorme
 
 import (
 	"database/sql"
@@ -51,4 +51,31 @@ func NewDocumentOrder(
 		DocumentID: documentID,
 		Page:       page,
 	}
+}
+
+const (
+	ClientRef  string = "client"
+	SessionRef string = "session"
+)
+
+type DocumentStore interface {
+	GetDocument(db DB, id int, clientID string) (*Document, error)
+	CreateDocument(db DB, doc *Document) (*Document, error)
+	UpdateDocument(db DB, ID int, doc *Document) (*Document, error)
+	DeleteDocument(db DB, ID int, clientID string) error
+	CheckDocumentsBelongToclients(db DB, clientIDs []string, documentIds []int) (bool, error)
+	RemoveAndRenumberDocumentsOrder(db DB, ID int, clientID string) error
+	CreateDocumentOrder(db DB, documentIds []int, refType string, refID string) ([]Document, error)
+	UpdateDocumentOrder(db DB, documentIds []int, refType string, refID string) ([]Document, error)
+	GetDocumentOrder(db DB, refType string, refID string) ([]Document, error)
+}
+
+type DocumentUseCase interface {
+	CreateDocument(clientID string, src string, name string, description string) (*Document, error)
+	UpdateDocument(clientID string, ID int, src string, name string, description string) (*Document, error)
+	DeleteDocument(clientID string, ID int) error
+	GetDocument(id int, clientID string) (*Document, error)
+	CreateDocumentOrder(clientID string, documentIds []int, refID string, refType string) ([]Document, error)
+	UpdateDocumentOrder(clientID string, documentIds []int, refID string, refType string) ([]Document, error)
+	GetDocumentOrder(clientID string, refID string, refType string) ([]Document, error)
 }
