c9ceffea255893fda20c3ca7bdb29f1f2e5bed5a
Arun Ranganathan <arun.ranga@hotmail.ca>
2021-03-05 22:58:49 -0500

    FEAT: session use cases

--- tutorme/session.go
+++ tutorme/session.go
@@ -12,6 +12,8 @@ type Session struct {
 	TutorID   string    `db:"tutor_id" json:"tutor_id"`
 	By        string    `db:"by" json:"by"`
 	RoomID    string    `db:"room_id" json:"room_id"`
+	Clients   []Client  `json:"clients"`
+	State     string    `db:"state" json:"state"`
 }
 
 type Event struct {
@@ -21,4 +23,47 @@ type Event struct {
 	Start     time.Time      `db:"start" json:"start"`
 	End       time.Time      `db:"end" json:"json"`
 	Title     sql.NullString `db:"title" json:"title"`
+	SessionId int            `db:"session`
+}
+
+// NewSession creates new Session
+func NewSession(
+	tutorID string,
+	by string,
+	roomID string,
+) *Session {
+	return &Session{
+		TutorID: tutorID,
+		By:      by,
+		RoomID:  roomID,
+	}
+}
+
+// NewEvent creates new Event
+func NewEvent(
+	start time.Time,
+	end time.Time,
+	title string,
+) *Event {
+	event := Event{Start: start, End: end}
+	if title != "" {
+		event.Title = sql.NullString{String: title, Valid: false}
+	}
+	return &event
+}
+
+type SessionStore interface {
+	GetSessionByClientID(db DB, clientID string, state string) (*[]Session, error)
+	GetSessionByRoomID(db DB, roomID string, state string) (*[]Session, error)
+	GetSessionByID(db DB, ID int) (*Session, error)
+	GetSessionByIDForUpdate(db DB, ID int) (*Session, error)
+	CheckSessionsIsForClient(db DB, client string, sessionIDs []int) (bool, error)
+	CheckOverlapingEvents(db DB, ID int, events *[]Event) (bool, error)
+	DeleteSession(db DB, ID int) error
+	UpdateSession(db DB, id int, by string, state string) (*Session, error)
+	CreateSession(db DB, session *Session) (*Session, error)
+	CreateSessionClients(db DB, sessionID int, clientIDs []string) (*[]Client, error)
+	CreateSessionEvents(db DB, events []Event) (*[]Event, error)
+	GetSessionEventFromClientIDs(db DB, clientIds []string, state bool) (*[]Event, error)
+	DeleteSessionEvents(db DB, IDs []int) error
 }
---
+++ usecases/session.go
@@ -0,0 +1,173 @@
+package usecases
+
+import (
+	"github.com/Arun4rangan/api-tutorme/tutorme"
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+)
+
+// SessionUseCase holds all business related functions for session
+type SessionUseCase struct {
+	DB           *sqlx.DB
+	SessionStore tutorme.SessionStore
+}
+
+func NewSessionUseCase(db sqlx.DB, sessionStore tutorme.SessionStore) *SessionUseCase {
+	return &SessionUseCase{&db, sessionStore}
+}
+
+func (su *SessionUseCase) CreateSession(
+	tutorID string,
+	by string,
+	roomID string,
+	clients []string,
+) (*tutorme.Session, error) {
+	session := tutorme.NewSession(tutorID, by, roomID)
+
+	tx, err := su.DB.Beginx()
+
+	if err != nil {
+		return nil, err
+	}
+
+	session, err = su.SessionStore.CreateSession(tx, session)
+
+	if err != nil {
+		if rb := tx.Rollback(); rb != nil {
+			return nil, errors.Wrap(rb, err.Error())
+		}
+		return nil, err
+	}
+
+	cl, err := su.SessionStore.CreateSessionClients(tx, session.ID, clients)
+
+	if err != nil {
+		if rb := tx.Rollback(); rb != nil {
+			return nil, errors.Wrap(rb, err.Error())
+		}
+		return nil, err
+	}
+
+	session.Clients = *cl
+
+	return session, nil
+}
+
+func (su SessionUseCase) UpdateSession(
+	id int,
+	by string,
+	state string,
+) (*tutorme.Session, error) {
+	tx, err := su.DB.Beginx()
+
+	session, err := su.SessionStore.GetSessionByIDForUpdate(su.DB, id)
+
+	if err != nil {
+		if rb := tx.Rollback(); rb != nil {
+			return nil, errors.Wrap(rb, err.Error())
+		}
+		return nil, err
+	}
+
+	//TODO: Add logic on what should be updated
+
+	updatedSession, err := su.SessionStore.UpdateSession(su.DB, id, by, state)
+
+	if err != nil {
+		if rb := tx.Rollback(); rb != nil {
+			return session, errors.Wrap(rb, err.Error())
+		}
+		return session, err
+	}
+
+	return updatedSession, nil
+}
+
+func (su SessionUseCase) GetSessionByID(clientID string, id int, roomID *string, state string) (*tutorme.Session, error) {
+	session, err := su.SessionStore.GetSessionByID(su.DB, id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	forClient, err := su.SessionStore.CheckSessionsIsForClient(su.DB, clientID, []int{session.ID})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !forClient {
+		return nil, errors.New("Session does not belong to client")
+	}
+	return session, nil
+}
+
+func (su SessionUseCase) GetSessionWithRoomId(clientID string, roomID string, state string) (*[]tutorme.Session, error) {
+	sessions, err := su.SessionStore.GetSessionByRoomID(su.DB, roomID, state)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var sessionIDs []int
+	for i := 0; i < len(*sessions); i++ {
+		sessionIDs = append(sessionIDs, (*sessions)[i].ID)
+	}
+
+	forClient, err := su.SessionStore.CheckSessionsIsForClient(su.DB, clientID, sessionIDs)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !forClient {
+		return nil, errors.New("Room does not belong to client")
+	}
+
+	return sessions, nil
+}
+
+func (su SessionUseCase) GetSessionWithClientID(clientID string, state string) (*[]tutorme.Session, error) {
+	return su.SessionStore.GetSessionByClientID(su.DB, clientID, state)
+}
+
+func (su SessionUseCase) DeleteSession(clientID string, ID int) error {
+	forClient, err := su.SessionStore.CheckSessionsIsForClient(su.DB, clientID, []int{ID})
+
+	if err != nil {
+		return err
+	}
+
+	if !forClient {
+		return errors.New("Session does not belong to client")
+	}
+
+	return su.SessionStore.DeleteSession(su.DB, ID)
+}
+
+func (su SessionUseCase) CreateSessionEvents(clientID string, ID int, events *[]tutorme.Event) (*[]tutorme.Event, error) {
+	// This will be a problem for the future because there is no guarantees that two parallel transaction will result in a unique event range
+	forClient, err := su.SessionStore.CheckSessionsIsForClient(su.DB, clientID, []int{ID})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !forClient {
+		return nil, errors.New("Session does not belong to client")
+	}
+
+	isOverLapping, err := su.SessionStore.CheckOverlapingEvents(su.DB, ID, events)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !isOverLapping {
+		return nil, errors.New("Events overlap")
+	}
+
+	insertedEvents, err := su.SessionStore.CreateSessionEvents(su.DB, *events)
+
+	return insertedEvents, err
+}
