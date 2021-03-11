2ef51c281386f16a80e0bde3aa608a2c451db384
Soory Ranganathan <soory.ranga@hotmail.com>
2021-03-11 07:17:46 -0500

    FEAT: Add session routes and views

--- migration/20210306215135_session_tables.up.sql
+++ migration/20210306215135_session_tables.up.sql
@@ -1,12 +1,12 @@
 BEGIN;
 
-CREATE TABLE IF NOT EXISTS session_event (
+CREATE TABLE IF NOT EXISTS scheduled_event (
   id SERIAL PRIMARY KEY,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   start TIMESTAMP NOT NULL,
   end TIMESTAMP NOT NULL,
-  title VARCHAR(40)
+  title VARCHAR(40),
 )
 
 CREATE TABLE IF NOT EXISTS tutor_session (
@@ -17,17 +17,8 @@ CREATE TABLE IF NOT EXISTS tutor_session (
   updated_by UUID REFERENCES client (id),
   room_id VARCHAR(40) NOT NULL,
   state VARCHAR(15) NOT NULL,
-  target_event_id INT REFERENCES event (id)
+  event_id INT REFERENCES scheduled_event (id)
 )
-
-ALTER TABLE IF EXISTS session_event 
-  ADD COLUMN session_id INT;
-
-ALTER TABLE session_event 
-   ADD CONSTRAINT fk_session_id
-   FOREIGN KEY (session_id) 
-   REFERENCES tutor_session ( id );
-
 
 CREATE TABLE IF NOT EXISTS session_client (
   id SERIAL PRIMARY KEY,
@@ -35,11 +26,19 @@ CREATE TABLE IF NOT EXISTS session_client (
   session_id INT REFERENCES tutor_session (id),
 )
 
-CREATE TABLE IF NOT EXISTS client_selected_event (
+CREATE TABLE IF NOT EXISTS client_selected_session (
   id SERIAL PRIMARY KEY,
+  can_attend BOOLEAN NOT NULL,
   client_id UUID REFERENCES client (id),
   session_id INT REFERENCES tutor_session (id),
-  event_id INT REFERENCES session_event (id)
+  UNIQUE (session_id, client_id)
+)
+
+CREATE TABLE IF NOT EXISTS client_event (
+  id SERIAL PRIMARY KEY,
+  client_id UUID REFERENCES client (id),
+  event_id INT REFERENCES scheduled_event (id)
+  UNIQUE (ref_type, ref_id, page)
 )
 
 COMMIT;
