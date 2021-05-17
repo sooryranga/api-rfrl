353d503bb9ef171d7f842149ab46b9276ecfb168
Soory Ranganathan <soory.ranga@hotmail.com>
2021-05-17 08:36:42 -0400

    FEAT: create code result firestore document in API

--- tutorme/firestore.go
+++ tutorme/firestore.go
@@ -6,4 +6,5 @@ type FireStoreClient interface {
 	CreateClient(id string, photo string, firstName string, lastName string) error
 	UpdateClient(id string, photo null.String, firstName null.String, lastName null.String) error
 	UpdateCode(sessionID int, codeID int, result string) error
+	CreateCode(sessionID int, codeID int) error
 }
