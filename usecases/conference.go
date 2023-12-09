a6197966bad25797d490ccd4c40c9a296f5b1818
Soory Ranganathan <soory.ranga@hotmail.com>
2023-12-08 22:24:02 -0500

    FEAT: simple-peer implementation for video chat

--- usecases/conference.go
+++ usecases/conference.go
@@ -3,6 +3,7 @@ package usecases
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -20,14 +21,6 @@ const (
 	PUBLISH     string = "publish"
 )
 
-type YjsSignallingMessage struct {
-	MessageType     string   `json:"type"`
-	SubscribeTopics []string `json:"topics"`
-	MessageTopic    string   `json:"topic"`
-	RawMessage      []byte
-	ConferenceID    string
-}
-
 type WebSocketClient struct {
 	hub *ConferenceHub
 
@@ -36,8 +29,19 @@ type WebSocketClient struct {
 	// The websocket connection.
 	conn *websocket.Conn
 
+	From string
+
 	// Buffered channel of outbound messages.
 	send chan []byte
+}
+
+type SignallingMessage struct {
+	MessageType     string   `json:"type"`
+	SubscribeTopics []string `json:"topics"`
+	RawMessage      []byte
+	ConferenceID    string
+	From            string
+	FromClient      *WebSocketClient
 }
 
 func (c *WebSocketClient) readPump() {
@@ -50,14 +54,16 @@ func (c *WebSocketClient) readPump() {
 		_, message, err := c.conn.ReadMessage()
 
 		if err != nil {
-			log.Errorj(log.JSON{"error": err.Error(), "conferenceID": c.ConferenceID})
+			log.Errorj(log.JSON{"error": err.Error(), "conferenceID": c.ConferenceID, "from": c.From})
 			return
 		}
 
-		var signalMessage YjsSignallingMessage
+		var signalMessage SignallingMessage
 		err = json.Unmarshal(message, &signalMessage)
 		signalMessage.RawMessage = message
 		signalMessage.ConferenceID = c.ConferenceID
+		signalMessage.From = c.From
+		signalMessage.FromClient = c
 
 		if err != nil {
 			log.Errorj(log.JSON{"error": err.Error(), "conferenceID": c.ConferenceID})
@@ -77,11 +83,11 @@ func (c *WebSocketClient) readPump() {
 	}
 }
 
-// writePump pumps messages from the hub to the websocket connection.
+// writePump pumps messages From the hub to the websocket connection.
 //
 // A goroutine running writePump is started for each connection. The
 // application ensures that there is at most one writer to a connection by
-// executing all writes from this goroutine.
+// executing all writes From this goroutine.
 func (c *WebSocketClient) writePump() {
 
 	ticker := time.NewTicker(tutorme.PingPeriod)
@@ -112,17 +118,6 @@ func (c *WebSocketClient) writePump() {
 
 			w.Write(message)
 
-			// Add queued chat messages to the current websocket message.
-			n := len(c.send)
-			for i := 0; i < n; i++ {
-				_, err = w.Write(<-c.send)
-
-				if err != nil {
-					log.Errorj(log.JSON{"error": err.Error(), "conferenceID": c.ConferenceID})
-				}
-
-			}
-
 			if err := w.Close(); err != nil {
 				log.Errorj(log.JSON{"error": err.Error(), "conferenceID": c.ConferenceID})
 				return
@@ -141,19 +136,19 @@ type ConferenceHub struct {
 	// Registered clients.
 	sessionConnectedClients map[string]map[*WebSocketClient]bool
 
-	// Inbound messages from the clients.
-	broadcast chan YjsSignallingMessage
+	// Inbound messages From the clients.
+	broadcast chan SignallingMessage
 
-	// Register requests from the clients.
+	// Register requests From the clients.
 	register chan *WebSocketClient
 
-	// Unregister requests from clients.
+	// Unregister requests From clients.
 	unregister chan *WebSocketClient
 }
 
 func NewConferenceHub() *ConferenceHub {
 	return &ConferenceHub{
-		broadcast:               make(chan YjsSignallingMessage),
+		broadcast:               make(chan SignallingMessage),
 		register:                make(chan *WebSocketClient),
 		unregister:              make(chan *WebSocketClient),
 		sessionConnectedClients: make(map[string]map[*WebSocketClient]bool),
@@ -166,17 +161,21 @@ func (h ConferenceHub) Run() {
 
 		case client := <-h.register:
 			conferenceID := client.ConferenceID
-			conferenceClients, ok := h.sessionConnectedClients[conferenceID]
+			from := client.From
+			id := fmt.Sprintf("%s-%s", from, conferenceID)
+			conferenceClients, ok := h.sessionConnectedClients[id]
 
 			if !ok {
 				conferenceClients = make(map[*WebSocketClient]bool)
-				h.sessionConnectedClients[conferenceID] = conferenceClients
+				h.sessionConnectedClients[id] = conferenceClients
 			}
 			conferenceClients[client] = true
 
 		case client := <-h.unregister:
 			conferenceID := client.ConferenceID
-			conferenceClients, ok := h.sessionConnectedClients[conferenceID]
+			from := client.From
+			id := fmt.Sprintf("%s-%s", from, conferenceID)
+			conferenceClients, ok := h.sessionConnectedClients[id]
 			if !ok {
 				continue
 			}
@@ -187,18 +186,24 @@ func (h ConferenceHub) Run() {
 
 		case message := <-h.broadcast:
 			conferenceID := message.ConferenceID
-			conferenceClients, ok := h.sessionConnectedClients[conferenceID]
+			from := message.From
+			id := fmt.Sprintf("%s-%s", from, conferenceID)
+			conferenceClients, ok := h.sessionConnectedClients[id]
 
 			if !ok {
 				continue
 			}
 
 			for client := range conferenceClients {
+				if client == message.FromClient {
+					continue
+				}
 				select {
 				case client.send <- message.RawMessage:
 				default:
-					close(client.send)
-					delete(conferenceClients, client)
+					log.Error("error")
+					// close(client.send)
+					// delete(conferenceClients, client)
 				}
 			}
 		}
@@ -229,12 +234,13 @@ func NewConferenceUseCase(
 	}
 }
 
-func (cu ConferenceUseCase) Serve(conn *websocket.Conn, conferenceID string) {
+func (cu ConferenceUseCase) Serve(conn *websocket.Conn, conferenceID string, from string) {
 	client := &WebSocketClient{
 		hub:          cu.Hub,
 		conn:         conn,
 		send:         make(chan []byte, 256),
 		ConferenceID: conferenceID,
+		From:         from,
 	}
 
 	client.hub.register <- client
